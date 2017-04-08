c96ec5ce8dd1ba523d3d9fac07c7e2fecc2ecf73
Master Owl <cheesytrevor@gmail.com>
2017-04-07 19:19:07 -0600

    Revert "Revert "Endgame screen now displays stats in dialog, no longer uses recyclerview""
    This reverts commit b598fb41bafa18ffde253ea0cecba6d2e0ab280a.

--- app/src/main/java/com/example/tyudy/ticket2rideclient/fragments/PlayerStatsDialog.java
+++ app/src/main/java/com/example/tyudy/ticket2rideclient/fragments/PlayerStatsDialog.java
@@ -3,14 +3,17 @@ package com.example.tyudy.ticket2rideclient.fragments;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Dialog;
+import android.app.FragmentManager;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.DialogFragment;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
 import android.widget.TextView;
 
 import com.example.tyudy.ticket2rideclient.R;
@@ -22,7 +25,6 @@ import com.example.tyudy.ticket2rideclient.common.UserStats;
  */
 
 public class PlayerStatsDialog extends DialogFragment {
-    private TextView mPlayerName;
     private TextView mTotalPoints;
     private TextView mRoutePoints;
     private TextView mCompletedDestPoints;
@@ -39,10 +41,22 @@ public class PlayerStatsDialog extends DialogFragment {
 
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
-        AlertDialog.Builder builder = new AlertDialog.Builder(mActivity);
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
         View v = mActivity.getLayoutInflater().inflate(R.layout.game_over_player_stats, null);
 
-        builder.setTitle(R.string.stats).
+        mRoutePoints = (TextView) v.findViewById(R.id.player_stats_route_points);
+        mCompletedDestPoints = (TextView) v.findViewById(R.id.player_stats_completed_dest);
+        mNotCompletedDest = (TextView) v.findViewById(R.id.player_stats_not_completed_dest);
+        mLongestRoutePoints = (TextView) v.findViewById(R.id.player_stats_longest_route);
+        mTotalPoints = (TextView) v.findViewById(R.id.player_stats_total_points);
+
+        mRoutePoints.setText(String.valueOf(mPlayer.getRoutePoints()));
+        mCompletedDestPoints.setText(String.valueOf(mPlayer.getDestPoints()));
+        mNotCompletedDest.setText(String.valueOf(mPlayer.getNegDestPoints()));
+        mLongestRoutePoints.setText(String.valueOf(mPlayer.getLongestRoutePoints()));
+        mTotalPoints.setText(String.valueOf(mPlayer.getTotalPoints()));
+
+        builder.setTitle(mPlayer.getName()).
                 setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialogInterface, int i) {
@@ -53,26 +67,5 @@ public class PlayerStatsDialog extends DialogFragment {
                 setView(v);
 
         return builder.create();
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.game_over_player_stats, container, false);
-
-        mPlayerName = (TextView) v.findViewById(R.id.player_stats_username);
-        mRoutePoints = (TextView) v.findViewById(R.id.player_stats_route_points);
-        mCompletedDestPoints = (TextView) v.findViewById(R.id.player_stats_completed_dest);
-        mNotCompletedDest = (TextView) v.findViewById(R.id.player_stats_not_completed_dest);
-        mLongestRoutePoints = (TextView) v.findViewById(R.id.player_stats_longest_route);
-        mTotalPoints = (TextView) v.findViewById(R.id.player_stats_total_points);
-
-        mPlayerName.setText(mPlayer.getName());
-        mRoutePoints.setText(String.valueOf(mPlayer.getRoutePoints()));
-        mCompletedDestPoints.setText(String.valueOf(mPlayer.getDestPoints()));
-        mNotCompletedDest.setText(String.valueOf(mPlayer.getNegDestPoints()));
-        mLongestRoutePoints.setText(String.valueOf(mPlayer.getLongestRoutePoints()));
-        mTotalPoints.setText(String.valueOf(mPlayer.getTotalPoints()));
-
-        return v;
     }
 }
