175414577c0ab50dee84ca3afb9b20f56df45a6a
Master Owl <cheesytrevor@gmail.com>
2017-04-07 19:08:37 -0600

    The end game screen now displays stats in a dialog and no longer uses RecyclerView :)

--- app/src/main/java/com/example/tyudy/ticket2rideclient/fragments/PlayerStatsDialog.java
+++ app/src/main/java/com/example/tyudy/ticket2rideclient/fragments/PlayerStatsDialog.java
@@ -1,6 +1,12 @@
 package com.example.tyudy.ticket2rideclient.fragments;
 
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.DialogFragment;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -15,7 +21,7 @@ import com.example.tyudy.ticket2rideclient.common.UserStats;
  * Created by Trevor on 4/3/2017.
  */
 
-public class PlayerStatsFragment extends Fragment {
+public class PlayerStatsDialog extends DialogFragment {
     private TextView mPlayerName;
     private TextView mTotalPoints;
     private TextView mRoutePoints;
@@ -23,9 +29,30 @@ public class PlayerStatsFragment extends Fragment {
     private TextView mLongestRoutePoints;
     private TextView mNotCompletedDest;
     private UserStats mPlayer;
+    private Activity mActivity;
 
     public void setUserStats(UserStats player){
         mPlayer = player;
+    }
+
+    public void setActivity(Activity a) { mActivity = a; }
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(mActivity);
+        View v = mActivity.getLayoutInflater().inflate(R.layout.game_over_player_stats, null);
+
+        builder.setTitle(R.string.stats).
+                setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.cancel(); // Close the dialog
+                    }
+                }).
+                setCancelable(false).
+                setView(v);
+
+        return builder.create();
     }
 
     @Override
